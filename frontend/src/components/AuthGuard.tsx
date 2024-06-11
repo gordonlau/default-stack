3cee228b42ba6d7e7a4de4fb38687523743b0a20
Gordon Lau Wai Chung <gordon@tecky.io>
2024-06-11 19:12:07 +0100

    Removed unused files

---
+++ frontend/src/components/AuthGuard.tsx
@@ -0,0 +1,19 @@
+import { Redirect } from "wouter"
+import { useAuthStore } from "../stores/auth"
+
+
+interface AuthGuardProps{
+    children: React.ReactElement
+}
+
+
+export function AuthGuard(props:AuthGuardProps){
+
+    const { isAuthenticated } = useAuthStore()
+
+	if (!isAuthenticated) {
+		return <Redirect to='/login' />
+	} else {
+		return <>{props.children}</>
+	}
+}
---
+++ frontend/src/stores/auth.ts
@@ -0,0 +1,16 @@
+
+
+import { create } from 'zustand'
+
+interface AuthState {
+	isAuthenticated: boolean
+	login: () => void
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+	isAuthenticated: false,
+	login() {
+		set({ isAuthenticated: true })
+		return true
+	}
+}))
