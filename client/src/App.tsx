fc5aa9b021c39efe839625239e9d115fadfecbd3
Gordon Lau Wai Chung <waichung@gordonlau.io>
2026-01-02 18:34:49 +0000

    Removed e2e and just import directly

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import { Route, Switch, useLocation } from 'wouter';
 import { Home } from './pages/Home';
 import { AuthGuard } from './components/AuthGuard';
+import { contract } from '../../api-server/src/contracts' 
 
 function App() {
     const [location] = useLocation();
+    console.log(contract)
     return (
         <div>
             <AuthGuard key={location}>
