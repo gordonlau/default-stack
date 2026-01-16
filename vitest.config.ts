aafccacdee2dec665d3fda458f6ab943384b79f6
Gordon Lau Wai Chung <waichung@gordonlau.io>
2026-01-16 23:17:22 +0000

    Add server-action mock to the default stack

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,15 @@
 import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({})
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            'astro:actions': new URL(
+                './src/__mocks__/astro-actions.ts',
+                import.meta.url
+            ).pathname,
+        },
+    },
+    test: { environment: 'happy-dom', globals: true },
+})
